6c1914d32d61dbd71fb647d60a9339d98e8706ad
nabin <nkbhaie2017@gmail.com>
2023-01-11 22:57:27 -0700

    refactor the code

--- cricket-ui/src/components/Constant.js
+++ cricket-ui/src/components/Constant.js
@@ -1,13 +1,5 @@
-// const BCRYPT_WORK_FACTOR = 12;
 const BCRYPT_WORK_FACTOR = process.env.PASS_WORK_FACTOR;
 // const BASE_URL = 'http://localhost:3000';
-// const GAMES_ENDPOINT = `${BASE_URL}/games`;
-// const RETRIEVE_ADMINS_URL = `${BASE_URL}/users/admins`;
-// const CREATE_ADMIN_ENDPOINT = `${BASE_URL}/users/admin`;
-// const PLAYERS_URL = `${BASE_URL}/players`;
-// const SIGN_UP_ENDPOINT = `${BASE_URL}/users`;
-// const LOGIN_ENDPOINT = `${BASE_URL}/user/login`;
-
 const BASE_URL = 'https://aecc-api.herokuapp.com';
 const GAMES_ENDPOINT = `${BASE_URL}/games`;
 const RETRIEVE_ADMINS_URL = `${BASE_URL}/users/admins`;
